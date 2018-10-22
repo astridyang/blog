923fdb9890e578b63c8a41d793ff534a76ad934d
sallyyoo <workyooll@gmail.com>
2018-10-22 11:44:02 +0800

    add mobile theme

--- blog/static/js/script.js
+++ blog/static/js/script.js
@@ -5,4 +5,14 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip(
         {title: render_time}
     );
+    var sidebar = $(".sidebar");
+    $("#J_open_cate").on('click',function (e) {
+        e.stopPropagation();
+        sidebar.toggleClass("show");
+    });
+    $("body").on('click', function (e) {
+        if(sidebar.hasClass('show')){
+            sidebar.removeClass("show");
+        }
+    })
 });
